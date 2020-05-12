4be70f772af84b171b3cde9786ba15a640b2990b
Jaime <jgaimer42@gmail.com>
2020-05-12 00:41:10 -0500

    cambios Jaime

--- models/jugador.py
+++ models/jugador.py
@@ -1,8 +1,14 @@
 import pygame
+from . import utilidades
 from . import constantes
+from .misil import Misil
 
-from .bala1 import Bala1
-
+#cambios realizados
+#1. creado funcionalidad pedida por JeyP de misiles laterales
+#2. la funcion recortar imagen se puso en el archivo utilidades
+#motivo: esto porque dicha funcion pude ser reusada en otros elementos del juego
+#3. la tecla para cambiar de sprite se cambio por la S
+#motivo: la tecla usada previamente esta ocupada por la nueva funcionalidad de disparo
 class Jugador(pygame.sprite.Sprite):
 
     def __init__(self,pos):
@@ -15,9 +21,9 @@ class Jugador(pygame.sprite.Sprite):
         sabana3 = pygame.image.load("./Sprites/PlayerShipSprite_III.png")
         self.velx = 0
         self.vely = 0
-        self.animaciones.append(self.recorte_imagen(sabana1,90,67))
-        self.animaciones.append(self.recorte_imagen(sabana2,80,85))
-        self.animaciones.append(self.recorte_imagen(sabana3,120,90))
+        self.animaciones.append(utilidades.recorte_imagen(sabana1,[90,67]))
+        self.animaciones.append(utilidades.recorte_imagen(sabana2,[80,85]))
+        self.animaciones.append(utilidades.recorte_imagen(sabana3,[120,90]))
         self.image = self.animaciones[self.estado][self.frame]
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
@@ -64,18 +70,21 @@ class Jugador(pygame.sprite.Sprite):
         if(evento.key == pygame.K_DOWN):
             self.vely = 5
             self.velx = 0
-        if(evento.key == pygame.K_a):
-            self.cambio_animacion()
         if(evento.key == pygame.K_s):
-            self.disparar(lista_balas)
+            self.cambio_animacion()
+        if(evento.key == pygame.K_a):
+            origen_disparo = [self.rect.right,self.rect.y]
+            self.disparar(lista_balas,origen_disparo)
+        if(evento.key == pygame.K_d):
+            origen_disparo = [self.rect.left,self.rect.y]
+            self.disparar(lista_balas,origen_disparo)
 
     def frenar(self):
         self.velx=0
         self.vely=0
 
-    def disparar(self,lista_balas):
-        origen_disparo = self.rect.center
-        bala = Bala1(origen_disparo)
+    def disparar(self,lista_balas,origen_disparo):
+        bala = Misil(origen_disparo)
         lista_balas.add(bala)
 
     def cambio_animacion(self):
@@ -86,10 +95,3 @@ class Jugador(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = pos_x
         self.rect.y = pos_y
-
-    def recorte_imagen(self,sabana,x,y):
-        animacion = []
-        for c in range(3):
-            cuadro = sabana.subsurface(x*c,0,x,y)
-            animacion.append(cuadro)
-        return (animacion)
