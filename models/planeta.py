8119dc80b2d2d706671037038dfbeb86196abaa9
JohnatanPalacios <39398211+JohnatanPalacios@users.noreply.github.com>
2020-05-24 21:10:36 -0500

    planeta y mas (#61)
    Co-authored-by: JottaP <jottapalacios@github.com>

--- models/planeta.py
+++ models/planeta.py
@@ -3,12 +3,21 @@ import random
 from . import constantes
 from .bloque_base import Bloque_base
 from . import ambiente
+from . import utilidades as util
 
 class Planeta(Bloque_base):
     def __init__(self,pos):
         super().__init__(pos)
+        self.frame = 0
         self.type = "planeta"
-        self.image.fill(constantes.AZUL)
-    
+        sabana = pygame.image.load("./Sprites/Planeta1.png")
+        self.animacion = util.recorte_explosion(sabana,[72,72],5,4)
+        self.image = self.animacion[self.frame]
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
     def update(self):
+        self.frame = util.animar(self.frame,19)
+        self.image = self.animacion[self.frame]
         super().update()
